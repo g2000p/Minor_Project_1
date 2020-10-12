1e468f1768687ee63c90dd06710897394c9e919b
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2020-10-12 18:43:38 +0530

    Add files via upload

---
+++ minor.c
@@ -0,0 +1,46 @@
+
+#include <dirent.h>
+#include<string.h>
+#include<stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+void getNames(); //function that is the heart of the program
+char * gettExtension(char *); //separate extension from file name
+
+filepath="";//enter the file path
+
+int main()
+{
+    getNames();
+    return(0);
+
+}
+
+
+char * gettExtension(char *fullname){
+    char* ext1;
+    ext1 = strrchr(fullname,'.');
+    return ext1+1;
+}
+
+
+void getNames(){
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(filepath);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            int dx;
+            int size=strlen(dir->d_name);
+            char filename[size+10];
+            char *ext;
+            strcpy(filename,dir->d_name);
+
+          }
+        closedir(d);
+}
+}
