dd75fc0fd93a967510b47807873938dc26e61cd3
Gourav Paul <43387305+g2000p@users.noreply.github.com>
2020-10-13 21:29:32 +0530

    Update minor.c

--- minor.c
+++ minor.c
@@ -1,14 +1,15 @@
-
+//the required header files
 #include <dirent.h>
-#include<string.h>
-#include<stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 
-void getNames(); //function that is the heart of the program
-char * gettExtension(char *); //separate extension from file name
 
-filepath="";//enter the file path
+void getNames(char []);     //gets the name of new folder that will be created
+char * gettExtension(char *);       //separates extension from file name
+
+filepath="";        //enter the file path
 
 int main()
 {
@@ -17,19 +18,20 @@ int main()
 
 }
 
-
-char * gettExtension(char *fullname){
+char * gettExtension(char *fullname)       //separates extension from file name
+{
     char* ext1;
     ext1 = strrchr(fullname,'.');
     return ext1+1;
 }
 
-
-void getNames(){
+void getNames()     //gets the name of new folder that will be created
+{
     DIR *d;
     struct dirent *dir;
     d = opendir(filepath);
-
+    char final_cmd[1000]="";
+    
     if (d)
     {
         while ((dir = readdir(d)) != NULL)
@@ -39,8 +41,23 @@ void getNames(){
             char filename[size+10];
             char *ext;
             strcpy(filename,dir->d_name);
-
           }
+         if(strcmpi(filename,".")==0||strcmpi(filename,"..")==0)continue;
+
+            if (searchFordot(filename)==1)
+            {
+                ext=gettExtension(filename);
+                dx=directory_exist(filepath,ext);
+                printf("\n%d %s\n",dx,filename);
+            }
+            else
+            {
+               ext=NULL;
+               printf("\n%s\n",filename);
+               continue;
+            }
+            chdir(filepath);
+        
         closedir(d);
-}
+    }
 }
