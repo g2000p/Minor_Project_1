ba3c879110678a13e76586b36006c472fe85846e
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2020-11-23 20:02:50 +0530

    Update contentf.h

--- contentf.h
+++ contentf.h
@@ -1,9 +1,14 @@
 #ifndef CONTENT_H_INCLUDED
 #define CONTENT_H_INCLUDED
 
-
+/*categorize the file according to the number like
+0.OTHER
+1.C
+2.JAVA
+*/
 int content_based_categorization(char []);
 
+//for counting the total keyword
 int count_of_keyword(char *x[]);
 
 
