7550c33532375885c7143f431c135061ac820822
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2020-12-10 20:34:40 +0530

    Update func.c

--- func.c
+++ func.c
@@ -156,4 +156,20 @@ char *intilizeitall(int argc, char* argv[]){
         GtkWidget *window, *button,*label;
         window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
         g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+   
+        button = gtk_button_new_with_label("Click me");
+        g_signal_connect(button, "clicked", G_CALLBACK(open_dialog), window);
+        gtk_container_set_border_width(GTK_CONTAINER(window), 200);
+        gtk_container_add(GTK_CONTAINER(window), button);
+
+
+
+
+        gtk_widget_show_all(window);
+        gtk_main();
+
+        return arr;
+    }
+
+
 
